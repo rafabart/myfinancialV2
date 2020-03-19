375ec8bd5accaf19f4d542c817005492a42b6e3a
Rafael Marinho <rafamola@gmail.com>
2020-03-19 11:03:50 -0300

    Add flyway to prod and dev db migration

--- src/main/java/com/myfinancial/model/config/DevConfig.java
+++ src/main/java/com/myfinancial/model/config/DevConfig.java
@@ -2,14 +2,14 @@ package com.myfinancial.model.config;
 
 import com.myfinancial.model.service.DataInitializer;
 import com.myfinancial.model.service.EmailService;
-import com.myfinancial.model.service.impl.SmtpEmailServiceImpl;
+import com.myfinancial.model.service.impl.MockEmailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("dev-local")
 public class DevConfig {
 
     @Autowired
@@ -24,6 +24,6 @@ public class DevConfig {
 
     @Bean
     public EmailService emailService() {
-        return new SmtpEmailServiceImpl();
+        return new MockEmailServiceImpl();
     }
 }
