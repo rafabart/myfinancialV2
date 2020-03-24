6b973416c10e4ceb4a7016b41135450078862ad7
Rafael Marinho <rafamola@gmail.com>
2020-03-24 15:47:26 -0300

    migration fix

--- src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
+++ src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
@@ -73,7 +73,12 @@ USE d6l5tr0on84g0a;
 
 
 insert INTO public.users (id, created_at,update_at,email,"name","password") VALUES
-(1,NOW(),NOW(),'rafamola@gmail.com','Rafael Marinho','$2a$10$pWPVaNp2d75OlUYRkH0mg.VYrgJNeChE9Z9f3CmC.FbTvyykKO5ZC');
+(1,NOW(),NOW(),'rafamola@gmail.com','Rafael Marinho','$2a$10$e2g5dw0.aBtx5EvxbrImeePyHk3BVqwUIlovOo6RORXKbHWO0zq5i');
+
+
+INSERT INTO public.user_profile_list (user_id,profile_list) VALUES
+(1,1)
+,(1,2);
 
 
 insert INTO public.category (id,created_at,update_at,"name",user_id) VALUES
