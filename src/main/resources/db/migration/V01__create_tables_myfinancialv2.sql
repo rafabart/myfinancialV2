72f4cc3b71643b4284f49b1741e285640df10417
Rafael Marinho <rafamola@gmail.com>
2020-03-24 15:15:05 -0300

    db name default

--- src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
+++ src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
@@ -69,6 +69,9 @@ create TABLE public.user_profile_list (
 );
 
 
+USE d6l5tr0on84g0a;
+
+
 insert INTO public.users (id, created_at,update_at,email,"name","password") VALUES
 (1,NOW(),NOW(),'rafamola@gmail.com','Rafael Marinho','$2a$10$pWPVaNp2d75OlUYRkH0mg.VYrgJNeChE9Z9f3CmC.FbTvyykKO5ZC');
 
