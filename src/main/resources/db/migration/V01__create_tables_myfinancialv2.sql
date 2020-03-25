3a786312c269d4bacacc66821456fdb4bc071baa
Rafael Marinho <rafamola@gmail.com>
2020-03-25 09:37:32 -0300

    Migration fixed

--- src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
+++ src/main/resources/db/migration/V01__create_tables_myfinancialv2.sql
@@ -67,20 +67,3 @@ create TABLE public.user_profile_list (
 	profile_list int4 NULL,
 	CONSTRAINT fkisn0eyxbbyq84upkg5smdbdkp FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-
-USE d6l5tr0on84g0a;
-
-
-insert INTO public.users (id, created_at,update_at,email,"name","password") VALUES
-(1,NOW(),NOW(),'rafamola@gmail.com','Rafael Marinho','$2a$10$e2g5dw0.aBtx5EvxbrImeePyHk3BVqwUIlovOo6RORXKbHWO0zq5i');
-
-
-INSERT INTO public.user_profile_list (user_id,profile_list) VALUES
-(1,1)
-,(1,2);
-
-
-insert INTO public.category (id,created_at,update_at,"name",user_id) VALUES
-(1,'2020-03-23 16:13:46.794','2020-03-23 16:13:46.794','Educação',1)
-,(2,'2020-03-23 16:13:46.828','2020-03-23 16:13:46.828','Diversão',1)
--- src/main/resources/db/migration/V02__insert_starters_values_myfinancialv2.sql
+++ src/main/resources/db/migration/V02__insert_starters_values_myfinancialv2.sql
@@ -1,6 +1,3 @@
-USE d6l5tr0on84g0a;
-
-
 insert INTO public.users (id, created_at,update_at,email,"name","password") VALUES
 (1,NOW(),NOW(),'rafamola@gmail.com','Rafael Marinho','$2a$10$e2g5dw0.aBtx5EvxbrImeePyHk3BVqwUIlovOo6RORXKbHWO0zq5i');
 
@@ -11,5 +8,5 @@ INSERT INTO public.user_profile_list (user_id,profile_list) VALUES
 
 
 insert INTO public.category (id,created_at,update_at,"name",user_id) VALUES
-(1,'2020-03-23 16:13:46.794','2020-03-23 16:13:46.794','Educação',1)
-,(2,'2020-03-23 16:13:46.828','2020-03-23 16:13:46.828','Diversão',1)
+(1,NOW(),NOW(),'Educação',1)
+,(2,NOW(),NOW(),'Diversão',1)
